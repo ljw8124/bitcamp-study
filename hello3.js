5e38460a4b104121cc3e7b01e2a4a4382be52ec0
ljw8124 <ljw8124@gmail.com>
2021-06-09 15:18:43 +0900

    infinite

--- hello3.js
+++ hello3.js
@@ -1,6 +1,6 @@
 cossole.log("무한도전 아세요?");
+console.log("저희가 많이 보죠");
 console.log("무한~");
 console.log("Hello, world!");
 console.log("무야호");
 console.log("그만큼 신나신다는거지..");
-console.log("저희가 많이 보죠");
