f8b5bd4b820b6ef42b4cac06d55310a36e2086f4
ljw8124 <ljw8124@gmail.com>
2021-06-09 16:35:24 +0900

    Change

--- hello201.js
+++ hello201.js
@@ -2,7 +2,9 @@ console.log("Hello1");
 console.log("Hello2");
 console.log("Hello3");
 console.log("Hello4");
-console.log("Hello5");
+
+console.log("Goodbye");
+
 console.log("Hello6");
 console.log("Hello7");
 console.log("Hello8");
